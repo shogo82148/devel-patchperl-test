b42fa4b60152a17e5ba9a3b6e19518e57265617a
ICHINOSE Shogo <shogo82148@gmail.com>
2025-09-02 23:56:38 +0900

    add patchperl job to build Devel::PatchPerl and update build-perl script

--- build-perl.pl
+++ build-perl.pl
@@ -1,6 +1,27 @@
 #!/usr/bin/env perl
 
+use v5.42;
+use utf8;
 use strict;
 use warnings;
+use Carp qw/croak/;
 
-1;
+my $perl_version = shift;
+
+sub execute_or_die(@cmd) {
+    my $cmd = join ' ', @cmd;
+    print STDERR "> $cmd\n";
+    my $code = system(@cmd);
+    if ($code != 0) {
+        croak "failed to execute $cmd: exit code $code";
+    }
+}
+
+execute_or_die("curl", "-sSL", "-o", "perl.tar.gz", "https://github.com/Perl/perl5/archive/refs/tags/v$perl_version.tar.gz");
+execute_or_die("tar", "-xvf", "perl.tar.gz");
+
+chdir("perl5-$perl_version") or croak "chdir failed: $!";
+execute_or_die("patchperl");
+execute_or_die("./Configure", "-des", "-Dusedevel");
+execute_or_die("make", "depend");
+execute_or_die("make");
